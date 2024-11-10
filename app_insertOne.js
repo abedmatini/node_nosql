93cc144571d93f99b7eca3933786c2f8ca5372ea
Abed.Matini <abed.matini@ogilvy.co.za>
2024-11-10 12:19:50 +0200

    gitignore added

---
+++ app_insertOne.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Employee = require('./employee');
+
+const uri =  "mongodb://localhost:27017/employeeDB";
+
+mongoose.connect(uri,{'dbName':'employeeDB'});
+
+//insertOne record into employee
+let newEmployee = new Employee({
+    emp_name: 'John Doe',
+    age: 37,
+    location: "Illinois",
+    email: "jdoe@somewhere.com"
+});
+newEmployee.save().then(function(){
+    Employee.find().then((data)=>{
+        console.log("\n\nDocuments in employees collection after insertOne")
+        console.log(data);
+        mongoose.connection.close();
+    })
+}).catch(function(error){
+    console.log(error)
+});
